a0adb13a5c1c755d9ddc1bd0fee5b66804904041
MBaumgartenBR <mbaumgartenbr@gmail.com>
2020-09-18 17:05:24 -0300

    Fix keyboard overlap textfield

--- lib/src/views/home/widgets/codearq_bottom_sheet.dart
+++ lib/src/views/home/widgets/codearq_bottom_sheet.dart
@@ -1,54 +1,68 @@
 part of '../home_view.dart';
 
-class CodearqEditor extends StatelessWidget {
+class CodearqEditor extends StatefulWidget {
+  @override
+  _CodearqEditorState createState() => _CodearqEditorState();
+}
+
+class _CodearqEditorState extends State<CodearqEditor> {
   final double bottomSheetHeight = 60;
   final _textController = TextEditingController();
 
   @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var homeManager = context.watch<HomeManager>();
-    return Container(
-      height: bottomSheetHeight,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(width: 8),
-          IconButton(
-            icon: const Icon(Icons.close),
-            tooltip: 'Cancelar',
-            splashRadius: 24,
-            onPressed: context.pop,
-          ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: TextField(
-              controller: _textController,
-              autofocus: true,
-              onSubmitted: (value) async {
-                await homeManager.saveCodearq(context, value);
-              },
-              style: context.theme().textTheme.headline5,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Digite um novo CODEARQ ...',
-                hintStyle: const TextStyle(fontStyle: FontStyle.italic),
+    return Padding(
+      padding: MediaQuery.of(context).viewInsets,
+      child: Container(
+        height: bottomSheetHeight,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 8),
+            IconButton(
+              icon: const Icon(Icons.close),
+              tooltip: 'Cancelar',
+              splashRadius: 24,
+              onPressed: context.pop,
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                controller: _textController,
+                autofocus: true,
+                onSubmitted: (value) async {
+                  await homeManager.saveCodearq(context, value);
+                },
+                style: context.theme().textTheme.headline5,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Digite aqui',
+                  hintStyle: const TextStyle(fontStyle: FontStyle.italic),
+                ),
               ),
             ),
-          ),
-          Container(
-            height: bottomSheetHeight,
-            child: FlatButton.icon(
-              icon: const Icon(Icons.check),
-              label: Text(
-                'SALVAR',
-                style: context.theme().textTheme.headline6,
+            Container(
+              height: bottomSheetHeight,
+              child: FlatButton.icon(
+                icon: const Icon(Icons.check),
+                label: Text(
+                  'SALVAR',
+                  style: context.theme().textTheme.headline6,
+                ),
+                onPressed: () async {
+                  await homeManager.saveCodearq(context, _textController.text);
+                },
               ),
-              onPressed: () async {
-                await homeManager.saveCodearq(context, _textController.text);
-              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
