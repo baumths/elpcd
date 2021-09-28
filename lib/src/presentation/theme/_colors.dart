e647b4d8f1c2964eabe67724ac384620061eb45d
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-09-28 16:43:38 -0300

    Adjusts app colors

--- lib/src/presentation/theme/_colors.dart
+++ lib/src/presentation/theme/_colors.dart
@@ -7,9 +7,6 @@ abstract class AppColors {
   static const Color primary = Color(0xFF00041A);
   static const Color primaryDarker = Color(0xFF00030F);
 
-  static const Color secondary = Color(0xFF31344B);
-  static const Color secondaryVariant = Color(0xFF202231);
-
   static const Color info = Color(0xFF0156B3);
   static const Color success = Color(0xFF29634B);
   static const Color warning = Color(0xFFF0B53C);
@@ -27,6 +24,6 @@ const ColorScheme kColorScheme = ColorScheme.light(
   surface: AppColors.white,
   onSurface: AppColors.primary,
   onSecondary: AppColors.primary,
-  secondary: AppColors.secondary,
-  secondaryVariant: AppColors.secondaryVariant,
+  secondary: AppColors.background,
+  secondaryVariant: AppColors.white,
 );
