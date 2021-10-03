7192375711f9ec98250edc1096d89ba0b33e2f9e
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-10-03 20:03:15 -0300

    Adds NavigationRail theme

--- lib/src/presentation/theme/theme.dart
+++ lib/src/presentation/theme/theme.dart
@@ -6,12 +6,39 @@ part '_metrics.dart';
 final ThemeData kLightTheme = ThemeData(
   colorScheme: kColorScheme,
   errorColor: AppColors.danger,
-  tooltipTheme: TooltipThemeData(
-    padding: const EdgeInsets.all(10),
-    textStyle: TextStyle(color: kColorScheme.onPrimary, fontSize: 12),
-    decoration: BoxDecoration(
-      color: kColorScheme.primary.withOpacity(.9),
-      borderRadius: AppBorderRadius.all,
-    ),
+  tooltipTheme: tooltipTheme,
+  navigationRailTheme: navigationRailTheme,
+);
+
+final TooltipThemeData tooltipTheme = TooltipThemeData(
+  padding: const EdgeInsets.all(AppEdgeInsets.small),
+  textStyle: TextStyle(color: kColorScheme.onPrimary, fontSize: 12),
+  decoration: BoxDecoration(
+    color: kColorScheme.primary.withOpacity(.9),
+    borderRadius: AppBorderRadius.all,
   ),
 );
+
+NavigationRailThemeData get navigationRailTheme {
+  final Color selectedColor = kColorScheme.onPrimary;
+  final Color unselectedColor = kColorScheme.onPrimary.withOpacity(.4);
+  const double iconSize = 32.0;
+
+  return NavigationRailThemeData(
+    backgroundColor: kColorScheme.primary,
+    selectedIconTheme: IconThemeData(
+      color: selectedColor,
+      size: iconSize,
+    ),
+    unselectedIconTheme: IconThemeData(
+      color: unselectedColor,
+      size: iconSize,
+    ),
+    selectedLabelTextStyle: TextStyle(
+      color: selectedColor,
+    ),
+    unselectedLabelTextStyle: TextStyle(
+      color: unselectedColor,
+    ),
+  );
+}
