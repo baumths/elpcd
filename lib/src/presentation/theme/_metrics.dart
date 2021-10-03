73305737c7e1eecabee195f9f41b2830776c154f
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-10-03 20:02:56 -0300

    Adds AppEdgeInsets to metrics

--- lib/src/presentation/theme/_metrics.dart
+++ lib/src/presentation/theme/_metrics.dart
@@ -19,3 +19,9 @@ abstract class AppBorderRadius {
   static const BorderRadius left = BorderRadius.horizontal(left: radius);
   static const BorderRadius right = BorderRadius.horizontal(right: radius);
 }
+
+abstract class AppEdgeInsets {
+  static const double small = 8;
+  static const double medium = 16;
+  static const double large = 24;
+}
