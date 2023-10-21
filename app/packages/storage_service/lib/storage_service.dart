8a2485739dc67fd02bbbf289f2b33c5828f6a649
baumths <baumths@gmail.com>
2023-10-21 11:06:08 -0300

    export in memory storage facade

--- app/packages/storage_service/lib/storage_service.dart
+++ app/packages/storage_service/lib/storage_service.dart
@@ -1,5 +1,6 @@
 library;
 
+export 'src/facades/memory/in_memory_facade.dart' show InMemoryStorageFacade;
 export 'src/features/edge.dart';
 export 'src/features/entity.dart';
 export 'src/storage_facade.dart';
