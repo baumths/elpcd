f1a8d192e6b5c82f88f1b993f7ac226749377bf1
baumths <baumths@gmail.com>
2023-10-29 16:44:38 -0300

    use path url strategy instead of hash

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:storage_service/storage_service.dart';
 
 import '/features/app/app.dart';
 
 void main() async {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
 
   final storageFacade = InMemoryStorageFacade();
