f9aad5e9d2081faad2ba2a1b5b6decff72c02384
baumths <baumths@gmail.com>
2023-11-20 10:38:37 -0300

    add a uuid gen static function to StorageFacade

--- app/packages/storage_service/lib/src/storage_facade.dart
+++ app/packages/storage_service/lib/src/storage_facade.dart
@@ -1,6 +1,12 @@
+import 'package:uuid/uuid.dart';
+
 import 'features/class.dart';
 
+const _uuid = Uuid();
+
 abstract class StorageFacade {
+  static String generateUniqueIdentifier() => _uuid.v7();
+
   Future<void> init() => Future.value();
   Future<void> dispose() => Future.value();
 
