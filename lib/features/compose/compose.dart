09b4edf8f66b3e6fb27f2337e099810ea9032321
baumths <baumths@gmail.com>
2024-07-28 10:08:01 -0300

    refactor function widgets

--- lib/features/compose/compose.dart
+++ lib/features/compose/compose.dart
@@ -27,7 +27,7 @@ class ComposeView extends StatelessWidget {
       )..add(
           ComposeStarted(classe),
         ),
-      child: BlocConsumer<ComposeBloc, ComposeState>(
+      child: BlocListener<ComposeBloc, ComposeState>(
         listenWhen: (p, c) => p.status != c.status,
         listener: (_, state) {
           if (state.status == ComposeStatus.success) {
@@ -42,42 +42,27 @@ class ComposeView extends StatelessWidget {
             );
           }
         },
-        buildWhen: (p, c) => p.isSaving != c.isSaving,
-        builder: (_, state) {
-          return BlocProvider<MetadataCubit>(
-            create: (_) => MetadataCubit(),
-            child: _ComposeViewScaffold(isSaving: state.isSaving),
-          );
-        },
+        child: BlocProvider<MetadataCubit>(
+          create: (_) => MetadataCubit(),
+          child: const ComposeScaffold(),
+        ),
       ),
     );
   }
 }
 
-class _ComposeViewScaffold extends StatelessWidget {
-  const _ComposeViewScaffold({required this.isSaving});
-
-  final bool isSaving;
+class ComposeScaffold extends StatelessWidget {
+  const ComposeScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 0.0,
-        title: BlocBuilder<ComposeBloc, ComposeState>(
-          buildWhen: (p, c) => p.isEditing != c.isEditing,
-          builder: (_, state) {
-            final l10n = AppLocalizations.of(context);
-            return Text(
-              state.isEditing
-                  ? l10n.editingClassComposeHeader
-                  : l10n.newClassComposeHeader,
-            );
-          },
-        ),
-        leading: _leading(context),
+        title: const ComposeHeader(),
+        leading: const DismissChangesIconButton(),
       ),
-      floatingActionButton: _floatingActionButton(context),
+      floatingActionButton: const SaveChangesFloatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
       body: Center(
         child: SizedBox(
@@ -85,7 +70,7 @@ class _ComposeViewScaffold extends StatelessWidget {
           child: BlocBuilder<ComposeBloc, ComposeState>(
             builder: (_, state) {
               return IgnorePointer(
-                ignoring: isSaving,
+                ignoring: state.isSaving,
                 child: const CustomScrollView(
                   slivers: [
                     SliverToBoxAdapter(child: SizedBox(height: 24)),
@@ -102,30 +87,65 @@ class _ComposeViewScaffold extends StatelessWidget {
       ),
     );
   }
+}
 
-  IconButton _leading(BuildContext context) {
+class DismissChangesIconButton extends StatelessWidget {
+  const DismissChangesIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return IconButton(
       tooltip: AppLocalizations.of(context).cancelButtonText,
       icon: const Icon(Icons.arrow_back),
-      onPressed: Navigator.of(context).pop,
+      onPressed: () => Navigator.pop(context),
     );
   }
+}
 
-  FloatingActionButton _floatingActionButton(BuildContext context) {
-    if (isSaving) {
-      return FloatingActionButton(
-        onPressed: null,
-        child: CircularProgressIndicator(
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
-      );
-    }
-    return FloatingActionButton.extended(
-      label: Text(AppLocalizations.of(context).saveButtonText.toUpperCase()),
-      icon: const Icon(Icons.check),
-      onPressed: () => context
-          .read<ComposeBloc>()
-          .add(SavePressed(metadata: context.read<MetadataCubit>().state)),
+class SaveChangesFloatingActionButton extends StatelessWidget {
+  const SaveChangesFloatingActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ComposeBloc, ComposeState>(
+      buildWhen: (p, c) => p.isSaving != c.isSaving,
+      builder: (context, state) {
+        if (state.isSaving) {
+          return FloatingActionButton(
+            onPressed: null,
+            child: CircularProgressIndicator(
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+          );
+        }
+        return FloatingActionButton.extended(
+          label: Text(
+            AppLocalizations.of(context).saveButtonText.toUpperCase(),
+          ),
+          icon: const Icon(Icons.check),
+          onPressed: () => context
+              .read<ComposeBloc>()
+              .add(SavePressed(metadata: context.read<MetadataCubit>().state)),
+        );
+      },
+    );
+  }
+}
+
+class ComposeHeader extends StatelessWidget {
+  const ComposeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    return BlocBuilder<ComposeBloc, ComposeState>(
+      buildWhen: (p, c) => p.isEditing != c.isEditing,
+      builder: (_, state) {
+        if (state.isEditing) {
+          return Text(l10n.editingClassComposeHeader);
+        }
+        return Text(l10n.newClassComposeHeader);
+      },
     );
   }
 }
--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../localization.dart';
+import '../../repositories/classes_repository.dart';
 import '../compose/compose.dart';
 import 'widgets/drawer.dart';
 import 'widgets/tree_view.dart';
@@ -15,49 +16,76 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).appTitle),
-        leading: _buildDrawerButton(),
-        actions: _buildActions(context),
+        leading: const OpenSettingsMenuButton(),
+        actions: const [
+          FindClassButton(),
+          AllClassesExpandedToggleButton(),
+          SizedBox(width: 8),
+        ],
       ),
-      floatingActionButton: _buildFAB(context),
+      floatingActionButton: const CreateClassFloatingActionButton(),
       drawer: const HomeDrawer(),
       body: ClassesTreeView(
-        repository: RepositoryProvider.of(context),
+        repository: RepositoryProvider.of<ClassesRepository>(context),
       ),
     );
   }
+}
 
-  List<Widget> _buildActions(BuildContext context) {
+class OpenSettingsMenuButton extends StatelessWidget {
+  const OpenSettingsMenuButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.segment),
+      tooltip: AppLocalizations.of(context).settingsButtonText,
+      onPressed: () => Scaffold.of(context).openDrawer(),
+    );
+  }
+}
+
+class FindClassButton extends StatelessWidget {
+  const FindClassButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.search),
+      tooltip: AppLocalizations.of(context).findClassTooltipMessage,
+      onPressed: null,
+    );
+  }
+}
+
+class AllClassesExpandedToggleButton extends StatelessWidget {
+  const AllClassesExpandedToggleButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     final controller = context.watch<TreeviewController>();
-    final expanded = controller.allNodesExpanded;
-    final l10n = AppLocalizations.of(context);
-    return [
-      IconButton(
-        icon: const Icon(Icons.search),
-        tooltip: l10n.findClassTooltipMessage,
-        onPressed: null,
-      ),
-      IconButton(
-        icon: Icon(expanded ? Icons.unfold_less : Icons.unfold_more),
-        tooltip: expanded
-            ? l10n.collapseAllClassesTooltipMessage
-            : l10n.expandAllClassesTooltipMessage,
-        onPressed: expanded ? controller.collapseAll : controller.expandAll,
-      ),
-      const SizedBox(width: 8),
-    ];
-  }
 
-  Widget _buildDrawerButton() {
-    return Builder(
-      builder: (BuildContext context) => IconButton(
-        icon: const Icon(Icons.segment),
-        tooltip: AppLocalizations.of(context).settingsButtonText,
-        onPressed: Scaffold.of(context).openDrawer,
-      ),
+    if (controller.allNodesExpanded) {
+      return IconButton(
+        icon: const Icon(Icons.unfold_less),
+        tooltip: AppLocalizations.of(context).collapseAllClassesTooltipMessage,
+        onPressed: controller.collapseAll,
+      );
+    }
+
+    return IconButton(
+      icon: const Icon(Icons.unfold_more),
+      tooltip: AppLocalizations.of(context).expandAllClassesTooltipMessage,
+      onPressed: controller.expandAll,
     );
   }
+}
 
-  Widget _buildFAB(BuildContext context) {
+class CreateClassFloatingActionButton extends StatelessWidget {
+  const CreateClassFloatingActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return FloatingActionButton.extended(
       label: Text(AppLocalizations.of(context).newClassButtonText),
       icon: const Icon(Icons.post_add),
