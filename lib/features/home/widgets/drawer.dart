7a1b8b593f9886fd0d0829dbb689ec27dcd99cc0
baumths <baumths@gmail.com>
2024-07-28 12:16:14 -0300

    new drawer header w/ project related links

--- lib/features/home/widgets/drawer.dart
+++ lib/features/home/widgets/drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../../localization.dart';
 import '../../backup/backup_section.dart';
 import '../../crosswalk/csv_export_list_tile.dart';
 import '../../settings/codearq.dart';
@@ -13,21 +16,69 @@ class HomeDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: const <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              image: DecorationImage(
-                image: AssetImage('assets/gedalogo_270x270.png'),
-              ),
-            ),
-            child: SizedBox.shrink(),
-          ),
+          Header(),
+          Divider(),
           CodearqListTile(),
           DarkModeSwitchListTile(),
           Divider(),
           CsvExportListTile(),
           Divider(),
           BackupSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  static final sourceCodeUrl = Uri.parse('https://github.com/baumths/elpcd');
+  static final blogUrl = Uri.parse('https://documentosdigitais.blogspot.com');
+
+  static Future<void> launch(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final l10n = AppLocalizations.of(context);
+    final colorFilter = ColorFilter.mode(
+      theme.colorScheme.onSurface,
+      BlendMode.srcIn,
+    );
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              AppLocalizations.of(context).appTitle,
+              style: theme.textTheme.headlineLarge,
+            ),
+          ),
+          IconButton(
+            tooltip: l10n.sourceCodeButtonTooltip,
+            onPressed: () => launch(sourceCodeUrl),
+            icon: SvgPicture.asset(
+              'assets/github-mark.svg',
+              height: 24,
+              colorFilter: colorFilter,
+            ),
+          ),
+          const SizedBox(width: 4),
+          IconButton(
+            onPressed: () => launch(blogUrl),
+            tooltip: l10n.opdsBlogButtonTooltip,
+            icon: SvgPicture.asset(
+              'assets/opds_icon.svg',
+              height: 24,
+              colorFilter: colorFilter,
+            ),
+          ),
         ],
       ),
     );
