f1d5ac1097f7108a097af8a3ca83487ecd32d6d3
baumths <baumths@gmail.com>
2024-08-16 16:50:03 -0300

    merge parts of tree_view.dart into main file

--- lib/features/home/tree_view.dart
+++ lib/features/home/tree_view.dart
@@ -9,8 +9,21 @@ import '../../../entities/classe.dart';
 import '../../../localization.dart';
 import '../../../repositories/classes_repository.dart';
 
-part 'tree_controller.dart';
-part 'tree_node.dart';
+class ClassesTreeViewController extends ChangeNotifier {
+  final treeController = TreeController(allNodesExpanded: false);
+
+  bool get allNodesExpanded => treeController.allNodesExpanded;
+
+  void expandAll() {
+    treeController.expandAll();
+    notifyListeners();
+  }
+
+  void collapseAll() {
+    treeController.collapseAll();
+    notifyListeners();
+  }
+}
 
 class ClassesTreeView extends StatefulWidget {
   const ClassesTreeView({super.key, required this.repository});
@@ -100,5 +113,78 @@ class _ClassesTreeViewState extends State<ClassesTreeView> {
     }
 
     return buildChildren(Classe.rootId) ?? <TreeNode>[];
+  }
+}
+
+class ClassesTreeViewNode extends StatelessWidget {
+  const ClassesTreeViewNode({
+    super.key,
+    required this.clazz,
+    required this.hasChildren,
+  });
+
+  final Classe clazz;
+  final bool hasChildren;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+    return Expanded(
+      child: InkWell(
+        onTap: () => navigator.showClassEditor(classId: clazz.id),
+        borderRadius: const BorderRadius.horizontal(left: Radius.circular(6)),
+        child: Row(
+          children: <Widget>[
+            const SizedBox(width: 8),
+            Expanded(
+              child: Text.rich(
+                TextSpan(
+                  children: <InlineSpan>[
+                    if (clazz.code.isNotEmpty) ...[
+                      TextSpan(
+                        text: clazz.code,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const TextSpan(text: ' - '),
+                    ],
+                    if (clazz.name.isEmpty)
+                      TextSpan(
+                        text: AppLocalizations.of(context).unnamedClass,
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      )
+                    else
+                      TextSpan(text: clazz.name),
+                  ],
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            if (!hasChildren)
+              IconButton(
+                tooltip: l10n.deleteButtonText,
+                color: theme.colorScheme.error,
+                icon: const Icon(Icons.delete),
+                onPressed: () async {
+                  final delete = await navigator.showWarningDialog(
+                    title: l10n.areYouSureDialogTitle,
+                    confirmButtonText: l10n.deleteButtonText,
+                  );
+                  if ((delete ?? false) && context.mounted) {
+                    final repository = context.read<ClassesRepository>();
+                    await repository.delete(clazz);
+                  }
+                },
+              ),
+            IconButton(
+              icon: const Icon(Icons.add),
+              tooltip: l10n.newSubordinateClassButtonText,
+              onPressed: () => navigator.showClassEditor(parentId: clazz.id),
+            ),
+            const SizedBox(width: 8),
+          ],
+        ),
+      ),
+    );
   }
 }
