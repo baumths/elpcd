caba0a61a3535edc9eb2358e6b3250597d6014d2
baumths <baumths@gmail.com>
2024-09-11 10:15:38 -0300

    switch to sqlite ClassesRepository impl

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,22 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'app/app.dart';
-import 'entities/classe.dart';
 import 'features/explorer/explorer.dart';
 import 'features/settings/settings_controller.dart';
 import 'repositories/classes_repository.dart';
 import 'shared/classes_store.dart';
+import 'sqlite3/sqlite3.dart' show openSqliteDatabase;
 
 Future<void> main() async {
   await Hive.initFlutter('.elpcd_database');
-  Hive.registerAdapter<Classe>(ClasseAdapter());
-
-  final classesBox = await Hive.openBox<Classe>('classes');
   final settingsBox = await Hive.openBox<Object>('settings');
+
+  final sqliteDatabase = await openSqliteDatabase();
 
   runApp(
     MultiProvider(
       providers: [
-        Provider<ClassesRepository>.value(
-          value: HiveClassesRepository(classesBox),
-        ),
+        Provider.value(value: SqliteClassesRepository(sqliteDatabase)),
         ChangeNotifierProvider(
           create: (context) => ClassesStore(
             repository: context.read<ClassesRepository>(),
